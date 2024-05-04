9605ae5b13bd517b5ac723fe84e02ed5373aaebc
unknown <philippediniz@outlook.com>
2024-05-04 16:45:33 -0300

    Criando minha conta Bancária terminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
+        //TODO: Conhecer e importar a classe Scanner
+
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Obter pelo Scanner os valores  digitados no terminal
+
+        // Exxibir a mensagem da conta criada
+
+       Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor digite o numero da conta: ");
+        int numConta = teclado.nextInt();
+
+        System.out.println("Por favor digite o numero da sua agência: ");
+        int NumAgencia = teclado.nextInt();
+
+        System.out.println("Por favor digite seu nome: ");
+        String nomeCliente = teclado.next();
+
+        System.out.println("Por favor Digite seu saldo: ");
+        double saldo = teclado.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + ",obrigado por criar uma conta em nosso banco,sua agência " + NumAgencia +
+                ",sua conta é " + numConta + " seu saldo " + saldo + " ja esta disponivel para saque.");
 
 
 
@@ -10,6 +37,9 @@ public class ContaTerminal {
 
 
 
-        System.out.println("Meu Projeto conta Banco");
+
+
+
+
     }
 }
