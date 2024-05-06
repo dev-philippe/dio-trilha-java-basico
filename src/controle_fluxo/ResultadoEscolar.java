7c8f8c7e073bc0948e4477c87161fd0c8b122116
unknown <philippediniz@outlook.com>
2024-05-06 14:00:19 -0300

    Condicional Encadeada

--- src/controle_fluxo/ResultadoEscolar.java
+++ src/controle_fluxo/ResultadoEscolar.java
@@ -2,12 +2,17 @@ package controle_fluxo;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 10;
 
         if(nota >= 7)
             System.out.println("Aprovado");
 
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova recuperação");
+
         else
-            System.out.println("Reprovado");
+        System.out.println("Reprovado");
+
+
     }
 }
