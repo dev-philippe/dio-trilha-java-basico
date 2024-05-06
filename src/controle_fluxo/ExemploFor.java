5d0bd1b6b7fc8cdc8a0c4d9b8c506e416949037f
unknown <philippediniz@outlook.com>
2024-05-06 15:40:18 -0300

    For

---
+++ src/controle_fluxo/ExemploFor.java
@@ -0,0 +1,12 @@
+package controle_fluxo;
+
+public class ExemploFor {
+    public static void main(String[] args) {
+        for ( int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+
+        System.out.println("joaozinho dormiu");
+
+    }
+}
