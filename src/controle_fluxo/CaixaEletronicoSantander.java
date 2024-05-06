0f8f16aae6e4d779de14cc840ac49859194e593f
unknown <philippediniz@outlook.com>
2024-05-06 12:36:02 -0300

    Condição Composta

--- src/controle_fluxo/CaixaEletronicoSantander.java
+++ src/controle_fluxo/CaixaEletronicoSantander.java
@@ -3,11 +3,15 @@ package controle_fluxo;
 public class CaixaEletronicoSantander {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double ValorSocilicitado = 17.0;
+        double ValorSocilicitado = 22.0;
 
-        if(ValorSocilicitado < saldo)
+        if(ValorSocilicitado < saldo) {
             saldo = saldo - ValorSocilicitado;
+            System.out.println("Novo saldo " + saldo);
 
-        System.out.println(saldo);
+        }else
+            System.out.println("Saldo insulficiente");
+
+
     }
 }
