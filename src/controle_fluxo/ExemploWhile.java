e09ef72e15a0b7598568433a125830658e7c690c
unknown <philippediniz@outlook.com>
2024-05-07 11:09:55 -0300

    ExemploDoWhile

--- src/controle_fluxo/ExemploWhile.java
+++ src/controle_fluxo/ExemploWhile.java
@@ -8,8 +8,10 @@ public class ExemploWhile {
 
         while (mesada > 0) {
             Double valorDoce = valorAleatorio();
+
             if (valorDoce > mesada)
                 valorDoce = mesada;
+
             System.out.println("Doce do valor: " + valorDoce + "Adicionado no carrinho");
             mesada = mesada - valorDoce;
         }
