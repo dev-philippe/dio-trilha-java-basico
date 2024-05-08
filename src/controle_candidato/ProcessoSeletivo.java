a1cd230a8b3ef90cb34299032c3280ef67281fa3
unknown <philippediniz@outlook.com>
2024-05-08 15:27:44 -0300

    Processo Seletivo Usando for e for each Imprimindo os selecionados

--- src/controle_candidato/ProcessoSeletivo.java
+++ src/controle_candidato/ProcessoSeletivo.java
@@ -4,11 +4,28 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
 
       /*  analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);*/
+    }
+
+    static  void imprimirSelecionados() {
+        String [] candidatos = {"PHILIPPE", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+
+        for(int indice = 0; indice < candidatos.length; indice++ ){
+            System.out.println("O candidato de n° " + (indice+1) + " é " + candidatos[indice]);
+
+        }
+
+        System.out.println("Forma abreviada de interação for each ");
+
+        for(String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi "+ candidato);
+        }
+
     }
 
     static void selecaoCandidatos() {
