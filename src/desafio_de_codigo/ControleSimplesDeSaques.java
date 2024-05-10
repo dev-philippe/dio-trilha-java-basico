8768dfaeb6e11f8a12edda948425eb6fa93008b7
unknown <philippediniz@outlook.com>
2024-05-10 00:30:34 -0300

    Desafio códigos Refatorando ControleSimplesDeSaques

--- src/desafio_de_codigo/ControleSimplesDeSaques.java
+++ src/desafio_de_codigo/ControleSimplesDeSaques.java
@@ -9,32 +9,25 @@ public class ControleSimplesDeSaques {
         System.out.println("Digite o limite diário de saque:");
         double limiteDiario = scanner.nextDouble();
 
-        // Loop while para iterar sobre os saques
-        while (true) {
-            System.out.println("Digite o valor do saque (ou 0 para encerrar):");
+        for (int i = 1; ; i++) {
+            System.out.println("Digite o valor do saque " + i + " (ou 0 para encerrar):");
             double valorSaque = scanner.nextDouble();
 
-            // Verifica se o valor do saque é zero, encerrando as transações
             if (valorSaque == 0) {
                 System.out.println("Transações encerradas.");
                 break;
             }
 
-            // Verifica se o valor do saque ultrapassa o limite diário
             if (valorSaque > limiteDiario) {
                 System.out.println("Valor do saque excede o limite diário. Transação não realizada.");
+                break;
             } else {
-                // Se o valor do saque não ultrapassar o limite diário, subtrai o valor do saque do limite diário
                 limiteDiario -= valorSaque;
                 System.out.println("Saque de R$ " + valorSaque + " realizado com sucesso.");
                 System.out.println("Limite diário restante: R$ " + limiteDiario);
             }
         }
 
-        // Fechamos o Scanner para evitar vazamento de recursos
         scanner.close();
     }
 }
-
-
-
